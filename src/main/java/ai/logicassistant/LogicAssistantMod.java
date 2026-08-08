8fd4c2e6623551a6a2620e4157951cf0deffc20c
nazik360 <nazarrusin19@gmail.com>
2026-08-08 04:24:23 +0300

    Remove constructor and log message from LogicAssistantMod

--- src/main/java/ai/logicassistant/LogicAssistantMod.java
+++ src/main/java/ai/logicassistant/LogicAssistantMod.java
@@ -3,8 +3,4 @@ package ai.logicassistant;
 import mindustry.mod.Mod;
 
 public class LogicAssistantMod extends Mod {
-
-    public LogicAssistantMod() {
-        System.out.println("[Logic Assistant] Mod loaded!");
-    }
 }
